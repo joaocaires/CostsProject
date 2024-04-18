43113c5fc8149545f165d93e352abf1626ab3f66
Joao Caires <jprcaires@gmail.com>
2024-04-17 21:46:25 -0300

    creation of data insertion via API

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,12 +1,34 @@
+import { useNavigate } from 'react-router-dom'
+
 import ProjectForm from '../project/ProjectForm'
 import styles from './NewProject.module.css'
 
 function NewProject(){
+    const navigate = useNavigate()
+
+    function createPost(project){
+        project.costs = 0
+        project.services = []
+
+        fetch('http://localhost:5000/projects', {
+            method: 'POST',
+            headers: {
+                'Content-type' : 'application/json'
+            },
+            body: JSON.stringify(project),
+        }).then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+            navigate('./projects', {message:'Projeto criado com sucesso'})
+        })
+        .catch(err => console.log(err))
+    }
+    
     return(
         <div className={styles.newproject_container}>
             <h1>Create project</h1>
             <p>Create your project and then add the services</p>
-            <ProjectForm btnText="New Project"/>
+            <ProjectForm handleSubmit={createPost} btnText="New Project"/>
         </div>
     )
 }
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -5,8 +5,9 @@ import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
-function ProjectForm({btnText}){
+function ProjectForm({ handleSubmit, btnText, projectData }){
     const [categories, setCategories] = useState([])
+    const [project, setProject] = useState(projectData || {})
 
     useEffect(() => {
         fetch("http://localhost:5000/categories", {
@@ -22,11 +23,34 @@ function ProjectForm({btnText}){
     .catch((err) => console.log(err))
     }, [])
     
+    const submit = (e) => {
+        e.preventDefault()
+        handleSubmit(project)
+    }
+
+    function handleChange(e){
+        setProject({...project, [e.target.name]: e.target.value })
+    }
+
+    function handleCategory(e){
+        setProject({...project, category: {
+            id: e.target.value,
+            name: e.target.options[e.target.selectedIndex].text,
+        }
+        })
+    }
+
     return( 
-        <form className={styles.form}>
-            <Input type="text" text="Project Name" name="name" placeholder="Enter a project name"/>
-            <Input type="number" text="Project budget" name="budget" placeholder="Enter a total budget"/>
-            <Select name="category_id" text="Enter a category" options={categories}/>
+        <form onSubmit={submit} className={styles.form}>
+            <Input type="text" text="Project Name" name="name" placeholder="Enter a project name" 
+            handleOnChange={handleChange} value={project.name ? project.name : ''} />
+
+            <Input type="number" text="Project budget" name="budget" placeholder="Enter a total budget" 
+            handleOnChange={handleChange} value={project.budget ? project.budget : ''} />
+
+            <Select name="category_id" text="Enter a category" options={categories} 
+            handleOnChange={handleCategory} value={project.category ? project.category.id : ''} />
+
             <SubmitButton text={btnText}/>
         </form>
     )
