b662e7f15f00dbce3e9a9466f6df291799ab34ed
Joao Caires <jprcaires@gmail.com>
2024-04-20 14:38:16 -0300

    data display

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -4,10 +4,10 @@ function Select({text, name, options, handleOnChange, value}){
     return(
         <div className={styles.form_control}>
             <label htmlFor={name}> {text}: </label>
-            <select name={name} id={name} onChange={handleOnChange} value={value || ''}>
+            <select required name={name} id={name} onChange={handleOnChange} value={value || ''}>
                 <option>Select an option</option>
                 {options.map((option) => (
-                    <option value={option.id} key={option.id}>
+                    <option required value={option.id} key={option.id}>
                        {option.name}
                     </option>
                 ))}
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,18 +1,52 @@
-import { useLocation } from 'react'
+import styles from './Projects.module.css'
+import { useLocation } from 'react-router-dom'
 
 import Message from "../layout/Message"
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton'
+import ProjectCard from '../project/ProjectCard'
+import { useState, useEffect } from 'react'
 
 function Projects(){
+
+    const [projects, setProjects] = useState([])
+
     const location = useLocation()
     let message = ''
     if(location.state){
         message = location.state.message
     }
 
+    useEffect(() => {
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type' : 'application/json',
+            },
+        })
+        .then(resp => resp.json())
+        .then(data => {console.log(data)})
+        .catch((err) => console.log(err))
+    }, [])
+
     return (
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>My Projects</h1>
+                <LinkButton to="/newproject" text="Create Projects"/>
+            </div>
+
             {message && <Message msg={message} type="sucess"/>}
+            <Container customClass="start">
+                {projects.length > 0 && projects.map((project) => (
+                <ProjectCard 
+                id={project.id}
+                name={project.name}
+                budget={project.budget}
+                category={project.category.name}
+                key={project.id}
+                />))}
+            </Container>
         </div>
     )
 }  
